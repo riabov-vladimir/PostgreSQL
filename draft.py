a84d5ee5c73112b1be930ef22b43b1166a26523b
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-07-29 23:42:29 +0300

    draft 4

--- draft.py
+++ draft.py
@@ -8,7 +8,7 @@ def create_db():  # создает таблицы
 	cur.execute('''
 	create table if not exists
 	Student4(
-	id serial PRIMARY KEY,
+	id integer primary key,
 	name varchar(100) not null,
 	gpa numeric(10,2),
 	birth TIMESTAMPTZ
@@ -18,7 +18,7 @@ def create_db():  # создает таблицы
 	cur.execute('''
 	create table if not exists
 	Course4(
-	id serial PRIMARY KEY,
+	id integer primary key,
 	name varchar(100) not null
 	);
 	''')
@@ -26,8 +26,9 @@ def create_db():  # создает таблицы
 	cur.execute('''
 	create table if not exists
 	StudentCourse4(
-	student_id INTEGER REFERENCES Student3(id),
-	course_id INTEGER REFERENCES  Course3(id)
+	id serial primary key,
+	student_id INTEGER REFERENCES Student4(id),
+	course_id INTEGER REFERENCES  Course4(id)
 	);
 	''')
 
@@ -44,7 +45,7 @@ def add_student(values: dict):
 	values)
 
 
-def add_students(course, values: list):
+def add_students(course_id, values: list):
 
 	global cur
 
@@ -56,9 +57,17 @@ def add_students(course, values: list):
 	values)
 
 	for student in values:
-		sid = student.get('id')
-		cur.execute("""INSERT INTO StudentInfo2(id, name) 
-			VALUES (%s,	%s)""",	(sid, course))
+		student_id = student.get('id')
+		cur.execute("""INSERT INTO StudentCourse4(student_id, course_id) 
+			VALUES (%s,	%s)""",	(student_id, course_id))
+
+
+def drop_bd():
+	cur.execute('''
+	drop table if exists Student4 CASCADE;
+	drop table if exists Course4 CASCADE;
+	drop table if exists StudentCourse4 CASCADE;
+	''')
 
 
 with pg.connect(database='riabowdb1', user='riabowdb1', password='1234567890',
@@ -66,47 +75,42 @@ with pg.connect(database='riabowdb1', user='riabowdb1', password='1234567890',
 
 	cur = conn.cursor()
 
-	# cur.execute('''
-	# drop table Student3 CASCADE;
-	# drop table Course3 CASCADE;
-	# drop table StudentCourse3 CASCADE;
-	# ''')
+	# drop_bd()
 
-	create_db()
+	# create_db()
 
-	courses = [{'course': 'python'}, {'course': 'java-script'}, {'course': 'PHP'}]
-
-	# cur.executemany("""INSERT INTO Course4(name)
-	# 	VALUES (%(course)s)""",	courses)
-
-
-	student1 = {'name': 'petya1', 'gpa': '66666666.66', 'birth': '01.01.2666'}
-	student2 = {'name': 'petya2', 'gpa': '66666666.66', 'birth': '01.01.2666'}
-	student3 = {'name': 'petya3', 'gpa': '66666666.66', 'birth': '01.01.2666'}
-	student4 = {'name': 'petya4', 'gpa': '66666666.66', 'birth': '01.01.2666'}
-	students = [student1, student2, student3, student4]
-	add_students('2', students)
-	vasya = {'name': 'vasya', 'gpa': '55555555.55', 'birth': '01.01.2666'}
-	add_student(vasya)
+	courses = [{'id': '1', 'course': 'python'}, {'id': '2', 'course': 'java-script'}, {'id': '3', 'course': 'PHP'}]
+	# cur.executemany("""INSERT INTO Course4(id, name)
+	# 	VALUES (%(id)s, %(course)s)""",	courses)
+	student1 = {'id': '1', 'name': 'petya1', 'gpa': '66666666.66', 'birth': '01.01.2666'}
+	student2 = {'id': '2', 'name': 'petya2', 'gpa': '66666666.66', 'birth': '01.01.2666'}
+	student3 = {'id': '3', 'name': 'petya3', 'gpa': '66666666.66', 'birth': '01.01.2666'}
+	student4 = {'id': '4', 'name': 'petya4', 'gpa': '66666666.66', 'birth': '01.01.2666'}
+	students1 = [student1, student2]
+	students2 = [student3, student4]
+	# add_students('1', students1)
+	add_students('2', students2)
+	vasya = {'id': '10', 'name': 'vasya', 'gpa': '55555555.55', 'birth': '01.01.2666'}
+	# add_student(vasya)
 
 
-	# cur.execute('''
-	# select * from Student3;
-	# ''')
-	#
-	# for s in cur.fetchall():
-	# 	print(s)
+	cur.execute('''
+	select * from Student4;
+	''')
 
-	# cur.execute('''
-	# select * from Course3;
-	# ''')
-	#
-	# for s in cur.fetchall():
-	# 	print(s)
+	for s in cur.fetchall():
+		print(s)
 
-	# cur.execute('''
-	# select * from StudentCourse3;
-	# ''')
-	#
-	# for s in cur.fetchall():
-	# 	print(s)
+	cur.execute('''
+	select * from Course4;
+	''')
+
+	for s in cur.fetchall():
+		print(s)
+
+	cur.execute('''
+	select * from StudentCourse4;
+	''')
+
+	for s in cur.fetchall():
+		print(s)
